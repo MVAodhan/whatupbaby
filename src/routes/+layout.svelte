7acb9acf8200b033ba5f0eac8a3582d763353e35
Aodhan <hamilton.aodhan@gmail.com>
2023-07-24 22:04:34 +1200

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import '../app.css';
+</script>
+
+<main class="w-screen h-screen flex justify-center items-center">
+	<slot />
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { Howl } from 'howler';
+
+	let sound = new Howl({
+		src: ['whatupbaby.mp3']
+	});
+</script>
+
+<button class="btn" on:click={() => sound.play()}>What up</button>
